06895f2eb85a1d0b1382a1bdc3784d03b208233c
Manik2375 <manikkpr2375@gmail.com>
2024-11-17 00:33:39 +0530

    Adding userID and password and adding logo

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,24 +1,59 @@
 import "./Form.css";
+import { useState } from "react";
 
 import TextField from "@mui/material/TextField";
 import SubmitButton from "@mui/material/Button";
 
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import XIcon from '@mui/icons-material/X';
+import XIcon from "@mui/icons-material/X";
 
 export default function Form() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="form-wrapper">
-      <section className="form">
-        <form>
-          <TextField id="username" label="Username" variant="outlined" />
-          <TextField id="password" label="Password" variant="outlined" />
+      <section className="form-sec">
+        <form
+          className="form"
+          onSubmit={(event) => {
+            if (username == "admin" && password == "admin") {
+              event.preventDefault();
+              window.location.href =
+                "https://manik2375.github.io/Hospital_Management_App/";
+            } else {
+              alert("The username/password in wrong!");
+              return;
+            }
+          }}
+        >
+          <TextField
+            required
+            id="username"
+            label="Username"
+            variant="outlined"
+            value={username}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <TextField
+            required
+            id="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            value={password}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(event.target.value);
+            }}
+          />
           <SubmitButton
             variant="contained"
             sx={{
               backgroundColor: "#002857",
             }}
+            type="submit"
           >
             Login
           </SubmitButton>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import "./Header.css"
+import logo from "../../assets/logo.svg";
 
 export default function Header() {
     return (
         <header className="page-header">
-           <h1>HMS  ¯\_(ツ)_/¯</h1>
+           <h1>HMS</h1>
+           <img src={logo} alt="" />
         </header>
     )
 }
